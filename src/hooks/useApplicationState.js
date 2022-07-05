6d3b6f5753e73f5dbce263174f7dbe1a2571dc9b
Arpit Jacob <arpit.jacob@gmail.com>
2022-07-05 12:36:47 +0200

    update/add package comment, add style

--- src/hooks/useApplicationState.js
+++ src/hooks/useApplicationState.js
@@ -3,18 +3,18 @@ import { useDeepCompareEffect } from "use-deep-compare";
 
 import usePerf from "./usePerf";
 import useApplicationReducer from "./useApplicationReducer";
-import htmlCssMapper from "../data/htmlcssmap.json"
+import htmlCssMapper from "../data/htmlcssmap.json";
 
 const _documents = [
-  // { 
+  // {
   //   selectors: { org: 'bcs', lang: 'hi', abbr: 'irv' },
   //   bookCode: 'tit',
   //   url: '/bcs-hi_irv.tit.usfm',
   // },
-  { 
-    selectors: { org: 'unfoldingWord', lang: 'en', abbr: 'ult' },
-    bookCode: 'psa',
-    url: '/unfoldingWord-en_ult.psa.usfm',
+  {
+    selectors: { org: "unfoldingWord", lang: "en", abbr: "ult" },
+    bookCode: "psa",
+    url: "/unfoldingWord-en_ult.psa.usfm",
   },
 ];
 
@@ -23,29 +23,39 @@ export default function useApplicationState(props) {
   const { verbose } = state;
 
   const { proskomma, stateId, newStateId } = useProskomma({ verbose });
-  const { done } = useImport({ proskomma, stateId, newStateId, documents: _documents });
+  const { done } = useImport({
+    proskomma,
+    stateId,
+    newStateId,
+    documents: _documents,
+  });
 
   const { catalog } = useCatalog({ proskomma, stateId, verbose });
 
-  const { id: docSetId, documents } = done && catalog.docSets[0] || {};
-  const { bookCode } = documents && documents[0] || {};
-  const ready = docSetId && bookCode || false;
+  const { id: docSetId, documents } = (done && catalog.docSets[0]) || {};
+  const { bookCode } = (documents && documents[0]) || {};
+  const ready = (docSetId && bookCode) || false;
   const isLoading = !done || !ready;
 
   const { state: perfState, actions: perfActions } = usePerf({
-    // proskomma, ready, docSetId, bookCode, verbose , htmlCssMapper  //uncomment if pasing custom classes. 
-    proskomma, ready, docSetId, bookCode, verbose
+    proskomma,
+    ready,
+    docSetId,
+    bookCode,
+    verbose,
+    htmlCssMapper, //uncomment if pasing custom classes.
+    // proskomma, ready, docSetId, bookCode, verbose
   });
   const { perfHtml } = perfState;
 
   useDeepCompareEffect(() => {
     if (perfHtml && perfHtml.mainSequenceId !== state.sequenceIds[0]) {
       actions.setSequenceIds([perfHtml?.mainSequenceId]);
-    };
+    }
   }, [perfHtml, state.sequenceIds]);
 
   return {
-    state: {...state, ...perfState, isLoading },
-    actions: {...actions, ...perfActions },
+    state: { ...state, ...perfState, isLoading },
+    actions: { ...actions, ...perfActions },
   };
-};
+}
--- src/hooks/usePerf.js
+++ src/hooks/usePerf.js
@@ -1,37 +1,59 @@
 import { useState, useTransition } from "react";
-import { useDeepCompareCallback, useDeepCompareEffect, useDeepCompareMemo } from "use-deep-compare";
-import isEqual from 'lodash.isequal';
+import {
+  useDeepCompareCallback,
+  useDeepCompareEffect,
+  useDeepCompareMemo,
+} from "use-deep-compare";
+import isEqual from "lodash.isequal";
 import PerfXHtml from "perfxhtml";
 
-
-export default function usePerf({ proskomma, ready, docSetId, bookCode, verbose, htmlCssMapper }) {
+export default function usePerf({
+  proskomma,
+  ready,
+  docSetId,
+  bookCode,
+  verbose,
+  htmlCssMapper,
+}) {
   const [isSaving, startSaving] = useTransition();
   const [perfHtml, setPerfHtml] = useState();
-  const epiPerfHtml = useDeepCompareMemo(() => (
-    ready && new PerfXHtml({ proskomma, docSetId, options: { historySize: 100 }})
-  ), [proskomma, ready, docSetId]);
+  const epiPerfHtml = useDeepCompareMemo(
+    () =>
+      ready &&
+      new PerfXHtml({ proskomma, docSetId, options: { historySize: 100 } }),
+    [proskomma, ready, docSetId]
+  );
 
   useDeepCompareEffect(() => {
     if (epiPerfHtml) {
-      epiPerfHtml.readHtml(bookCode,htmlCssMapper).then((_perfHtml) => {  //remove htmlCssMapper for default classes
+      epiPerfHtml.readHtml(bookCode, htmlCssMapper).then((_perfHtml) => {
+        //remove htmlCssMapper for default classes
         setPerfHtml(_perfHtml);
       });
     }
   }, [epiPerfHtml, bookCode]);
 
-  const savePerfHtml = useDeepCompareCallback(({ sequenceId, sequenceHtml }) => {
-    let _perfHtml = { ...perfHtml };
-    _perfHtml.sequencesHtml[sequenceId] = sequenceHtml;
+  const savePerfHtml = useDeepCompareCallback(
+    ({ sequenceId, sequenceHtml }) => {
+      let _perfHtml = { ...perfHtml };
+      _perfHtml.sequencesHtml[sequenceId] = sequenceHtml;
 
-    if (!isEqual(perfHtml, _perfHtml)) setPerfHtml(_perfHtml);
-    
-    startSaving(async () => {
-      const newPerfHtml = await epiPerfHtml?.writeHtml( bookCode, sequenceId, _perfHtml );
-      if (verbose) console.log({ info: "Saved sequenceId", bookCode, sequenceId });
-  
-      if (!isEqual(perfHtml, newPerfHtml)) setPerfHtml(newPerfHtml);
-    });
-  }, [perfHtml, bookCode]);
+      if (!isEqual(perfHtml, _perfHtml)) setPerfHtml(_perfHtml);
+
+      startSaving(async () => {
+        const newPerfHtml = await epiPerfHtml?.writeHtml(
+          bookCode,
+          sequenceId,
+          _perfHtml
+        );
+        if (verbose)
+          console.log({ info: "Saved sequenceId", bookCode, sequenceId });
+
+        if (!isEqual(perfHtml, newPerfHtml)) setPerfHtml(newPerfHtml);
+      });
+    },
+    [perfHtml, bookCode]
+  );
 
   const undo = async () => {
     const newPerfHtml = await epiPerfHtml?.undoHtml(bookCode);
@@ -43,8 +65,10 @@ export default function usePerf({ proskomma, ready, docSetId, bookCode, verbose,
     setPerfHtml(newPerfHtml);
   };
 
-  const canUndo = epiPerfHtml?.canUndo && epiPerfHtml?.canUndo(bookCode) || false;
-  const canRedo = epiPerfHtml?.canRedo && epiPerfHtml?.canRedo(bookCode) || false;
+  const canUndo =
+    (epiPerfHtml?.canUndo && epiPerfHtml?.canUndo(bookCode)) || false;
+  const canRedo =
+    (epiPerfHtml?.canRedo && epiPerfHtml?.canRedo(bookCode)) || false;
 
   const state = {
     bookCode,
@@ -57,7 +81,7 @@ export default function usePerf({ proskomma, ready, docSetId, bookCode, verbose,
   const actions = {
     savePerfHtml,
     undo,
-    redo
+    redo,
   };
 
   return { state, actions };
