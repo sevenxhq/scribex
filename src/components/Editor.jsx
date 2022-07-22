c0faa4fc7ac5c6ed1b71ce08585aa46a08cc92a9
samueljd <samuelj.ernest@gmail.com>
2022-07-22 18:41:14 +0530

    scribex with 2.0  schema

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -4,29 +4,60 @@ import { useContext } from "react";
 import { AppContext } from "../hooks/App.context";
 import useLifecycleLog from "../hooks/useLifecycleLog";
 
-import EditorWrapper from "./EditorWrapper";
-import PerfEditorWrapper from "./PerfEditorWrapper";
+import Section from "./Section";
+import SectionHeading from "./SectionHeading";
+import SectionBody from "./sectionBody";
+import { HtmlPerfEditor } from "@xelah/type-perf-html";
 
 export default function Editor() {
-  const { state: { sequenceIds, isSaving, isLoading } } = useContext(AppContext);
+  const {
+    state: {
+      sequenceIds,
+      isSaving,
+      isLoading,
+      htmlPerf,
+      sectionable,
+      blockable,
+      editable,
+      preview,
+      verbose,
+    },
+    actions: { addSequenceId, saveHtmlPerf },
+  } = useContext(AppContext);
   const sequenceId = sequenceIds.at(-1);
 
   useLifecycleLog(Editor);
   const style = (isSaving || isLoading || !sequenceId) ? { cursor: 'progress' } : {};
 
-  // const skeleton = (
-  //   <Stack spacing={1}>
-  //     <Skeleton key='1' variant="text" height="8em" sx={{ bgcolor: 'white' }} />
-  //     <Skeleton key='2' variant="rectangular" height="16em" sx={{ bgcolor: 'white' }} />
-  //     <Skeleton key='3' variant="text" height="8em" sx={{ bgcolor: 'white' }} />
-  //     <Skeleton key='2' variant="rectangular" height="16em" sx={{ bgcolor: 'white' }} />
-  //   </Stack>
-  // )
+  const props = {
+    htmlPerf: htmlPerf,
+    onHtmlPerf: saveHtmlPerf,
+    sequenceIds,
+    addSequenceId,
+    components: {
+      section: Section,
+      sectionHeading: SectionHeading,
+      sectionBody: SectionBody,
+    },
+    options: {
+      sectionable,
+      blockable,
+      editable,
+      preview
+    },
+    // handlers: {
+    //   onSectionClick,
+    //   onBlockClick
+    // },
+    decorators: {},
+    verbose,
+  };
+
 
   return (
     <div className="editor" style={style}>
       {!sequenceId && <p>loading</p> }
-      {sequenceId && <EditorWrapper sequenceId={sequenceId} /> }
+      {sequenceId && <HtmlPerfEditor {...props} /> }
     </div>
   );
 };
